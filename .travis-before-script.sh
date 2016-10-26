94af9d3fa73dc7df53cfc01b00e9a819de56e762
Steve Oliver <steve@circatree.com>
2016-10-26 16:59:38 -0700

    Download commerce with drush.

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -6,5 +6,14 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
+# Download commerce
+(
+    # These variables come from environments/drupal-*.sh
+    mkdir -p "$DRUPAL_TI_MODULES_PATH"
+    cd "$DRUPAL_TI_MODULES_PATH"
+
+    git clone --branch 8.x-2.x http://git.drupal.org/project/commerce.git
+)
+
 # Turn on PhantomJS for functional Javascript tests
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
