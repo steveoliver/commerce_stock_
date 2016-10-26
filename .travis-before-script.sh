1b9aa2fdcfaaeaf5c299516d3e6fd44e62a1c817
Steve Oliver <steve@circatree.com>
2016-10-26 16:51:25 -0700

    Use travis files from commerce module.

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -6,31 +6,5 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
-# Add custom modules to drupal build.
-cd "$DRUPAL_TI_DRUPAL_DIR"
-
-# Download custom branches of certain modules.
-(
-    # These variables come from environments/drupal-*.sh
-    mkdir -p "$DRUPAL_TI_MODULES_PATH"
-    cd "$DRUPAL_TI_MODULES_PATH"
-
-    git clone --branch 8.x-1.x http://git.drupal.org/project/address.git
-    git clone --branch 8.x-1.x http://git.drupal.org/project/entity.git
-    git clone --branch 8.x-1.x http://git.drupal.org/project/inline_entity_form.git
-    git clone --branch 8.x-1.x http://git.drupal.org/project/state_machine.git
-    git clone --branch 8.x-1.x http://git.drupal.org/project/profile.git
-)
-
-# Ensure the module is linked into the codebase.
-drupal_ti_ensure_module_linked
-
-# Update composer.
-composer update -n --lock --verbose
-
-# Enable main module and submodules.
-drush dl -y commerce --dev
-drush en -y commerce commerce_product commerce_order commerce_checkout commerce_stock
-
 # Turn on PhantomJS for functional Javascript tests
 phantomjs --ssl-protocol=any --ignore-ssl-errors=true $DRUPAL_TI_DRUPAL_DIR/vendor/jcalderonzumba/gastonjs/src/Client/main.js 8510 1024 768 2>&1 >> /dev/null &
