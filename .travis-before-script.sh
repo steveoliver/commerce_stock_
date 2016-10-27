90b8e245ac492bebe98579a6c92bdaa8ff8f1705
Steve Oliver <steve@circatree.com>
2016-10-26 17:07:46 -0700

    Download commerce dependencies.

--- .travis-before-script.sh
+++ .travis-before-script.sh
@@ -6,12 +6,17 @@ set -e $DRUPAL_TI_DEBUG
 # Note: This function is re-entrant.
 drupal_ti_ensure_drupal
 
-# Download commerce
+# Download commerce and dependencies.
 (
     # These variables come from environments/drupal-*.sh
     mkdir -p "$DRUPAL_TI_MODULES_PATH"
     cd "$DRUPAL_TI_MODULES_PATH"
 
+    git clone --branch 8.x-1.x http://git.drupal.org/project/address.git
+    git clone --branch 8.x-1.x http://git.drupal.org/project/entity.git
+    git clone --branch 8.x-1.x http://git.drupal.org/project/inline_entity_form.git
+    git clone --branch 8.x-1.x http://git.drupal.org/project/state_machine.git
+    git clone --branch 8.x-1.x http://git.drupal.org/project/profile.git
     git clone --branch 8.x-2.x http://git.drupal.org/project/commerce.git
 )
 
